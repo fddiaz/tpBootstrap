724cdb1eb98509a2f9cb6b78e497c1bda28cf3ac
Federico Diaz <1410288+fddiaz@users.noreply.github.com>
2023-07-12 16:05:16 -0300

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,9 @@
 <?php
 
-echo("<h1>Select * FROM rols</h1>");
-
 require_once("../includes/dbconnect.php");
 
+echo("<h1>Select * FROM rols</h1>");
+echo $connection->host_info . "<br>";
 $sql = "SELECT * FROM rols";
 $result = $connection->query($sql);
 
